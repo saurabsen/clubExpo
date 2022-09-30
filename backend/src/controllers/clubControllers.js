7df97a35e9cbf993050c638fe18fd74f99545772
saurabs@patriot1tech.com <saurav540@yahoo.com>
2022-09-30 12:10:11 -0700

    basic crud with mongo

--- backend/src/controllers/clubControllers.js
+++ backend/src/controllers/clubControllers.js
@@ -1,10 +1,13 @@
-const { asyncHandler } = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
+
+const Club = require("../models/clubModel");
 
 // @desc get clubs
 // @route GET /api/clubs
 // @access Private
 const getClubs = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Club" });
+  const clubs = await Club.find();
+  res.status(200).json(clubs);
 });
 
 // @desc set clubs
@@ -15,21 +18,41 @@ const setClubs = asyncHandler(async (req, res) => {
     res.status(400).json({ message: "Please sent text" });
     // throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "Set Club" });
+
+  const club = await Club.create({
+    text: req.body.text,
+  });
+  res.status(200).json(club);
 });
 
 // @desc get clubs
 // @route PUT /api/clubs/:id
 // @access Private
 const updateClubs = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update Club ${req.params.id}` });
+  const club = await Club.findById(req.params.id);
+  if (!club) {
+    res.status(400);
+    throw new Error("Club not found");
+  }
+
+  const updatedClub = await Club.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedClub);
 });
 
 // @desc get clubs
 // @route Delete /api/clubs/:id
 // @access Private
 const deleteClubs = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete Club ${req.params.id}` });
+  const club = await Club.findById(req.params.id);
+  if (!club) {
+    res.status(400);
+    throw new Error("Club not found");
+  }
+
+  await club.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
