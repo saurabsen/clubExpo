42502cd7bd3408fb606ca33c2af773563e8f5b3d
Pavan Soratur <pavanvsoratur@gmail.com>
2022-10-13 14:16:00 -0700

    updates user model and user controllers with JWT auth

--- backend/src/models/clubModel.js
+++ backend/src/models/clubModel.js
@@ -1,20 +1,70 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const clubSchema = mongoose.Schema(
   {
-    user: {
+    name: {
+      type: String,
+      required: [true, 'Please enter club name'],
+    },
+    logoImage: {
+      type: String,
+    },
+    coverImage: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter club description'],
+    },
+    createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
-      ref: "User",
+      ref: 'User',
     },
-    text: {
+    admins: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
+    ],
+    acceptedMembers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    pendingMembers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    events: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+      },
+    ],
+    badges: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Badge',
+      },
+    ],
+    status: {
       type: String,
-      required: [true, "Please add a text"],
+      required: [true, `Please enter club's status - active or archived`],
     },
+    tags: [
+      {
+        type: String,
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = mongoose.model("Club", clubSchema);
+module.exports = mongoose.model('Club', clubSchema);
