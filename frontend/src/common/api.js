eca081d3bf627ef8af7f9278f156b3078f22f5a4
Harshit Punn <harshitpunn@Harshits-MacBook-Air.local>
2022-10-16 22:27:15 -0700

    component-frontend-structure: ADDED components and views structure to project

---
+++ frontend/src/common/api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API = () => {
+
+  const getAPI =  (queryString,params) => {
+    return axios.get(`/api/${queryString}`, params);
+  }
+
+  const postAPI =  (queryString,params) => {
+    return axios.post(`/api/${queryString}`, params);
+  }
+
+  const putAPI =  (queryString,params) => {
+    return axios.put(`/api/${queryString}`, params);
+  }
+
+  const deleteAPI =  (queryString,params) => {
+    return axios.delete(`/api/${queryString}`, params);
+  }
+
+}
+
+export default API;
+
+export const events = () => {
+  const getEvents = (params) => {
+    return API.get(`events`,params);
+  }
+
+  const postEvents = (params) => {
+    return API.post(`events`,params);
+  }
+
+  const updateEvents = (params) => {
+    return API.delete(`events`,params);
+  }
+
+  const deleteEvents = (params) => {
+    return API.delete(`events`,params);
+  }
+
+}
---
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,11 @@
+import './pagination.css';
+
+const Pagination = () => {
+  return (
+    <div className='pagination'>
+      <h2>Pagination</h2>
+    </div>
+  );
+};
+
+export default Pagination;
