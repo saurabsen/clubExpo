8cd545b53fa12a3785786efd4e263fa858e0d2d8
Edward <epfrndz@gmail.com>
2022-10-21 11:31:45 -0700

    Finished upcoming events component

--- frontend/src/components/UpcomingEvents/UpcomingEvents.jsx
+++ frontend/src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,9 +1,76 @@
 import './upcomingevents.css';
 
-const UpcomingEvents = () => {
+const UpcomingEvents = ({ upcomingEvents }) => {
+  const numberToMonth = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+  };
+
+  const longToShortMonth = {
+    "January": "Jan",
+    "February": "Feb",
+    "March": "Mar",
+    "April": "Apr",
+    "May": "May",
+    "June": "Jun",
+    "July": "Jul",
+    "August": "Aug",
+    "September": "Sep",
+    "October": "Oct",
+    "November": "Nov",
+    "December": "Dec"
+  };
+
+  let sortedEvents = [...upcomingEvents].sort((a, b) => {
+    return a.eventDate - b.eventDate;
+  });
+
+  let monthCounter;
+  let monthShortname;
+
+  const checkForMonth = (currentDate) => {
+    if (1 + currentDate.getMonth() !== monthCounter) {
+      monthCounter = 1 + currentDate.getMonth();
+      monthShortname = longToShortMonth[numberToMonth[monthCounter]];
+      return <p className="monthname">{numberToMonth[monthCounter]}</p>;
+    }
+  };
+
+
   return (
     <div className="upcomingevents">
-      <h2>Upcoming Events</h2>
+      <h4>Upcoming Events</h4>
+
+      {sortedEvents.map((event, i) => {
+        const eventDate = event.eventDate;
+
+        return (
+          <>
+            {checkForMonth(eventDate)}
+            <div className="event-container">
+              <div className="event-date">
+                <p><span>{monthShortname}</span><span><br/>{eventDate.getDate()}</span></p>
+              </div>
+              <a className="event-title" href={event.eventLink}>
+                <p>{event.eventName}</p>
+              </a>
+              <a className="clubname" href={event.eventLink}>
+                <p>{event.clubName}</p>
+              </a>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };
