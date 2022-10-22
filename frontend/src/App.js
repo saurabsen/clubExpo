a4da0b636de7b576fe89207393f29333c7cc3a53
Pavan Soratur <pavanvsoratur@gmail.com>
2022-10-21 19:03:16 -0700

    adds header, search and sidebar components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './views/Home/Home';
+import { Header } from './components';
+import { Home } from './views';
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (searchResults) => {
+    setSearchResults(searchResults);
+  };
+
   return (
     <>
       <Router>
-        <Header />
+        <Header handleSearch={handleSearch} />
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
