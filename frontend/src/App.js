924d3db2b6b2e562036f12af4626c4616ae4794f
Harshit Punn <harshitpunn@Harshits-MacBook-Air.local>
2022-10-22 16:28:11 -0700

    grid-system: ADDED code to fix grid system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components';
 import { Home, ClubsJoined,DiscoverClubs } from './views';
+import SideBar from './components/Sidebar/SideBar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -13,14 +16,24 @@ const App = () => {
   return (
     <>
       <Router>
+      <Box sx={{ flexGrow: 1 }}>
+      
+        <Grid container>
+        <Grid item xs={12}>
         <Header handleSearch={handleSearch} />
-        <div>
+          </Grid>
+          <Grid item xs={2}>
+          <SideBar userRole="member" />
+          </Grid>
+          <Grid item xs={10}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/clubs-joined" element={<ClubsJoined />} />
             <Route path="/discover-clubs" element={<DiscoverClubs />} />
           </Routes>
-        </div>
+          </Grid>
+        </Grid>
+      </Box>
       </Router>
     </>
   );
--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -3,18 +3,8 @@ import './home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="column-1">
-        <SideBar userRole="member" />
-      </div>
-
-      <div className="column-2">Events Cards</div>
-
-      <div className="column-3">
-        <div>Upcoming Events</div>
-        <div>Calender</div>
-      </div>
-    </div>
+    <>
+    </>
   );
 };
 
