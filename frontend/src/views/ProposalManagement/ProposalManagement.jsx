d608f2e69211dd6348034b00d0914600a1d074f0
saurabs@patriot1tech.com <saurav540@yahoo.com>
2022-10-29 21:47:39 -0400

    get data fixed

--- frontend/src/views/ProposalManagement/ProposalManagement.jsx
+++ frontend/src/views/ProposalManagement/ProposalManagement.jsx
@@ -3,12 +3,10 @@ import axios from 'axios';
 import './proposalManagement.css';
 
 const ProposalManagement = () => {
-  const [allProposals, setAllProposals] = useState();
+  const [allProposals, setAllProposals] = useState([]);
   let proposals = [];
   useEffect(() => {
-    const temp = getProposals('getproposals');
-    setAllProposals(temp);
-    console.log(allProposals, 'all proposl');
+    getProposals('getproposals');
   }, []);
   const tokenStr =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzUwMmRiMzRhMjcwYmY0ZDFhMjc5MWIiLCJpYXQiOjE2NjYxOTg5NjMsImV4cCI6MTY2ODc5MDk2M30.lPOmtB9fdmlIhDIj_R4yAvnt04ZWmuReNPNESVAak_8';
@@ -22,15 +20,14 @@ const ProposalManagement = () => {
       headers: { Authorization: `Bearer ${tokenStr}` }
     });
     const temp = data.data;
-    proposals = temp;
-    return proposals;
+    console.log(temp, 'data');
+    setAllProposals(temp);
   };
 
   return (
     <div>
       test
-      {proposals.map((element) => {
-        console.log(proposals, 'ui');
+      {allProposals.map((element) => {
         return <div>{element.clubName}test</div>;
       })}
     </div>
