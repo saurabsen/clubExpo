8eb1098a0932dddb9be33d4c5cac7109bd5a455d
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-10-30 23:05:15 -0700

    Modified MUI theme, added route for event page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components';
-import { Home, ClubsJoined,DiscoverClubs } from './views';
-import SideBar from './components/Sidebar/SideBar';
+import '@fontsource/raleway';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+
+import { Header } from './components';
+import { Home, ClubsJoined, DiscoverClubs, EventPage } from './views';
+import SideBar from './components/Sidebar/SideBar';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
@@ -13,29 +17,53 @@ const App = () => {
     setSearchResults(searchResults);
   };
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#8658CE',
+        main: '#5D1EBF',
+        dark: '#8658CE',
+        contrastText: '#fff'
+      }
+    },
+    typography: {
+      fontFamily: ['Raleway', 'serif'].join(','),
+      button: {
+        textTransform: 'none',
+        fontWeight: 500,
+        fontFamily: 'Raleway'
+      }
+    }
+  });
+
+  const UserEventsPage = () => {
+    let { eventId } = useParams();
+    return <EventPage eventId={eventId}/>;
+  };
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
-      <Box sx={{ flexGrow: 1 }}>
-      
-        <Grid container>
-        <Grid item xs={12}>
-        <Header handleSearch={handleSearch} />
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container>
+            <Grid item xs={12}>
+              <Header handleSearch={handleSearch} />
+            </Grid>
+            <Grid className='app-sidebar' item xs={2}>
+              <SideBar userRole="member" />
+            </Grid>
+            <Grid item xs={10}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="events/:eventId" element={<UserEventsPage />} />
+                <Route path="/clubs-joined" element={<ClubsJoined />} />
+                <Route path="/discover-clubs" element={<DiscoverClubs />} />
+              </Routes>
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
-          <SideBar userRole="member" />
-          </Grid>
-          <Grid item xs={10}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/clubs-joined" element={<ClubsJoined />} />
-            <Route path="/discover-clubs" element={<DiscoverClubs />} />
-          </Routes>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
       </Router>
-    </>
+    </ThemeProvider>
   );
 };
 
