c96ad40b3826a09c0a56dc1e57fe5416ead9a8c2
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-10-30 23:05:43 -0700

    Added events page to exports

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,5 +1,6 @@
 import Home from './Home/Home';
+import EventPage from './EventPage/EventPage';
 import ClubsJoined from './ClubsJoined/ClubsJoined';
 import DiscoverClubs from './DiscoverClubs/DiscoverClubs';
 
-export { Home, ClubsJoined, DiscoverClubs };
+export { Home, ClubsJoined, DiscoverClubs, EventPage };
