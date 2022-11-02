ede61f40dcd6b81301fa910e55e691d62853b6f4
Pavan Soratur <pavanvsoratur@gmail.com>
2022-11-01 19:35:46 -0700

    resolves merge conflicts

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import SearchBar from './SearchBar/SearchBar';
+import SideBar from './Sidebar/SideBar';
 
-export { Header, Footer, SearchBar };
+export { Header, Footer, SearchBar, SideBar };
