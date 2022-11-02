c58f4fdb08c9fa4f956c3f1fb24925b7e799f5aa
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-11-01 22:02:50 -0700

    Club cards now render with data from DB

--- frontend/src/components/ClubCard/ClubCard.jsx
+++ frontend/src/components/ClubCard/ClubCard.jsx
@@ -5,23 +5,27 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-const ClubCard = () => {
+const ClubCard = ({
+  clubImage="https://picsum.photos/id/237/200/300",
+  clubName="Placeholder",
+  clubNumMembers=0
+}) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         height="140"
-        image="https://picsum.photos/id/237/200/300"
-        alt="green iguana"
+        image={clubImage}
+        alt={`Feature image for club ${clubName}`}
       />
       <CardContent>
         <Typography variant="h5" component="div">
-          Club 1
+          {clubName}
         </Typography>
       </CardContent>
       <CardActions>
         <Typography gutterBottom component="div" sx={{pl:1}}>
-          20 members
+          {clubNumMembers} {clubNumMembers !== 1 ? "members" : "member"}
         </Typography>
       </CardActions>
     </Card>
