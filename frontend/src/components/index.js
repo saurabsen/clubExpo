1537e5cd1b532d073b0d13e9f084bc14d745fa94
Pavan Soratur <pavanvsoratur@gmail.com>
2022-11-03 14:53:48 -0700

    completes individual proposal request UI

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer/Footer';
 import SearchBar from './SearchBar/SearchBar';
 import SideBar from './Sidebar/SideBar';
 import Button from './Button/Button';
+import ProposalDetailCard from './ProposalDetailCard/ProposalDetailCard';
 
-export { Header, Footer, SearchBar, SideBar, Button };
+export { Header, Footer, SearchBar, SideBar, Button, ProposalDetailCard };
