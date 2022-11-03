899aeda070c3be3e3f751dad102c5391e345ea99
Pavan Soratur <pavanvsoratur@gmail.com>
2022-11-03 12:54:28 -0700

    complete proposal creation flow

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,6 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import SearchBar from './SearchBar/SearchBar';
 import SideBar from './Sidebar/SideBar';
+import Button from './Button/Button';
 
-export { Header, Footer, SearchBar, SideBar };
+export { Header, Footer, SearchBar, SideBar, Button };
--- frontend/src/views/ClubRequests/ClubRequests.jsx
+++ frontend/src/views/ClubRequests/ClubRequests.jsx
@@ -1,52 +1,57 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import './clubrequests.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '../../components/Button/Button';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button
+} from '@mui/material';
+// import { Button } from '../../components';
+import { useActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
 
 const ClubRequests = () => {
   const [allProposals, setAllProposals] = useState([]);
+  const navigate = useNavigate();
+
+  const { getProposalByStatus } = useActions();
+  const { data } = useTypedSelector((state) => state.proposals);
+
   useEffect(() => {
-    getProposals('getproposals');
+    getProposalByStatus();
   }, []);
-  const tokenStr =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzUwMmRiMzRhMjcwYmY0ZDFhMjc5MWIiLCJpYXQiOjE2NjYxOTg5NjMsImV4cCI6MTY2ODc5MDk2M30.lPOmtB9fdmlIhDIj_R4yAvnt04ZWmuReNPNESVAak_8';
 
-  const status = {
-    statusArray: 'Pending'
-  };
+  useEffect(() => {
+    if (data) {
+      setAllProposals(data);
+    }
+  }, [data]);
 
-  const getProposals = async (queryString) => {
-    const data = await axios.post(`http://localhost:3001/api/proposals/${queryString}`, status, {
-      headers: { Authorization: `Bearer ${tokenStr}` }
-    });
-    const temp = data.data;
-    console.log(temp, 'data');
-    setAllProposals(temp);
-  };
-
-  const viewProposal = (e) => {
-    console.log(e);
+  const viewProposal = (id) => {
+    const proposal = allProposals.filter((proposal) => proposal._id === id);
+    if (proposal[0]) {
+      navigate(`/proposals/${id}`);
+    }
   };
 
   return (
-    <div>
-      <h2>All Clubs</h2>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+    <div style={{ padding: '2rem 0' }}>
+      <h3>Club Requests</h3>
+      <br />
+      <TableContainer component={Paper} sx={{ maxWidth: 'fit-content' }}>
+        <Table sx={{ minWidth: 650 }} size="medium" aria-label="a dense table">
           <TableHead className="table-head">
             <TableRow>
-              <TableCell>Clubname</TableCell>
-              <TableCell>Admin</TableCell>
-              <TableCell align="center">No. of Members</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell></TableCell>
+              <TableCell align="center">Clubname</TableCell>
+              <TableCell align="center">Admin</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Description</TableCell>
+              <TableCell align="center">Details</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -56,15 +61,39 @@ const ClubRequests = () => {
                 key={row._id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
+                <TableCell component="th" scope="row" align="center">
                   {row.clubName}
                 </TableCell>
-                <TableCell>{row.createrName}</TableCell>
-                <TableCell align="center">{row.members.length}</TableCell>
-                <TableCell>{row.approvalStatus}</TableCell>
-                <TableCell onClick={() => viewProposal(row._id)} align="right">
-                  <Button variant="contained" innerText="View Detail">
-                    View Detail
+                <TableCell align="center">{row.creatorName}</TableCell>
+                <TableCell align="center">{row.approvalStatus}</TableCell>
+                <TableCell align="center">
+                  {row.description.length > 80 ? (
+                    <>
+                      {row.description.substring(0, 80)}...
+                      <b>
+                        <a href="#" onClick={() => viewProposal(row._id)}>
+                          {' '}
+                          Read more
+                        </a>
+                      </b>
+                    </>
+                  ) : (
+                    row.description
+                  )}
+                </TableCell>
+                <TableCell align="center">
+                  <Button
+                    sx={{
+                      backgroundColor: '#5D1EBF',
+                      fontSize: '0.9rem',
+                      '&:hover': {
+                        backgroundColor: '#5D1EBF'
+                      }
+                    }}
+                    variant="contained"
+                    onClick={() => viewProposal(row._id)}
+                  >
+                    View Details
                   </Button>
                 </TableCell>
               </TableRow>
