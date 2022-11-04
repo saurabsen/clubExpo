711c61f3b380d35b5c082b6c4c214e15989700c5
saurabs@patriot1tech.com <saurav540@yahoo.com>
2022-11-04 11:36:51 -0700

    local commit

---
+++ frontend/src/state/search/types.js
@@ -0,0 +1,3 @@
+export const FETCH_SEARCH_BY_CLUBS = '@proposals/getSearchByClubs';
+export const FETCH_SEARCH_BY_CLUBS_DATA_SUCCESS = '@proposals/getSearchByClubsDataSuccess';
+export const FETCH_SEARCH_BY_CLUBS__DATA_ERROR = '@proposals/getSearchByClubsDataError';
