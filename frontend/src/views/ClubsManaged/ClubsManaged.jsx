70050cafb668f40895ac5c43fb5e252cf1893ebe
saurabs@patriot1tech.com <saurav540@yahoo.com>
2022-11-09 01:08:56 -0800

    reoved console logs

--- frontend/src/views/ClubsManaged/ClubsManaged.jsx
+++ frontend/src/views/ClubsManaged/ClubsManaged.jsx
@@ -10,30 +10,32 @@ const ClubsManaged = () => {
   const clubs = useTypedSelector((state) => state.clubs);
 
   useEffect(() => {
-    debugger;
     const userID = JSON.parse(localStorage.getItem('user'));
     getClubsDataByUser(userID._id);
-    console.log(clubs, 'data');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2} sx={{ p: 2 }}>
-          {clubs.data.map((clubData) => (
-            <Grid key={clubData.createdAt} item xs={4}>
-              <ClubCard
-                key={clubData._id}
-                clubImage={clubData.logoImage}
-                clubName={clubData.name}
-                clubNumMembers={clubData?.acceptedMembers?.length}
-                clubId={clubData._id}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <div style={{ padding: '2rem 0' }}>
+        <h3>Club Requests</h3>
+        <br />
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2} sx={{ p: 2 }}>
+            {clubs.data.map((clubData) => (
+              <Grid key={clubData.createdAt} item xs={4}>
+                <ClubCard
+                  key={clubData._id}
+                  clubImage={clubData.logoImage}
+                  clubName={clubData.name}
+                  clubNumMembers={clubData?.acceptedMembers?.length}
+                  clubId={clubData._id}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </div>
     </>
   );
 };
