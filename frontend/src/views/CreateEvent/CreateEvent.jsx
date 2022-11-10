40dc7a6f90b867db6d9756c6438a3e51e72f5e12
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-11-10 10:55:21 -0800

    Added styling

--- frontend/src/views/CreateEvent/CreateEvent.jsx
+++ frontend/src/views/CreateEvent/CreateEvent.jsx
@@ -18,6 +18,7 @@ import {
   Radio,
   Button
 } from '@mui/material';
+import { BackButton } from '../../components';
 
 const CreateEvent = (props) => {
   const { clubId } = props;
@@ -71,113 +72,147 @@ const CreateEvent = (props) => {
     }
   };
 
+  const cardStyle = {
+    border: '1px solid #E0E2E0',
+    boxShadow: 'unset',
+    py: '16px',
+    px: '24px',
+    pb: '26px',
+    '& .MuiFormControl-root': {
+      mt: '12px'
+    },
+    '& .MuiFormGroup-root': {
+      mt: '12px'
+    }
+  };
+
+  const buttonStyle = {
+    // py: '16px',
+    fontSize: '16px',
+    flexGrow: '1',
+    maxWidth: '190px'
+  };
+
   return (
     <>
       <Card>
-        <CardMedia
-          component="img"
-          height={'194px'}
-          image={'https://picsum.photos/id/910/800/200'}
-        />
-        <Typography variant="h4">Create Event</Typography>
-        <Box
-          component="form"
-          onSubmit={onSubmitHandler}
-          sx={{
-            '& > :not(style)': { m: 1 }
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <Typography>Event name</Typography>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="formEventName">Enter event name</InputLabel>
-            <Input required id="formEventName" onChange={onChangeHandler} name="name" />
-          </FormControl>
-
-          <Typography>Select date and time</Typography>
-
-          <FormControl variant="standard">
-            <TextField
-              required
-              id="formEventDateTime"
-              variant="standard"
-              type="datetime-local"
-              name="startDate"
-              sx={{ width: 250 }}
-              InputLabelProps={{
-                shrink: true
-              }}
-              onChange={onChangeHandler}
-            />
-          </FormControl>
-
-          <Typography></Typography>
-          <FormControl>
-            <FormLabel id="demo-radio-buttons-group-label">Location</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="Online"
-              name="type"
-              onChange={onChangeHandler}
-            >
-              <FormControlLabel value="Online" control={<Radio />} label="Online" />
-              <FormControlLabel value="Venue" control={<Radio />} label="Venue" />
-            </RadioGroup>
-            <FormControl variant="standard">
-              <InputLabel htmlFor="formLinkOrAddress">Platform or Address</InputLabel>
-              <Input id="formLinkOrAddress" onChange={onChangeHandler} name="location" />
-            </FormControl>
-          </FormControl>
-
-          <Typography>Event description</Typography>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="formEventDescription">Describe your event</InputLabel>
-            <Input id="formEventDescription" onChange={onChangeHandler} name="description" />
-          </FormControl>
-
-          <Typography>Max attendees</Typography>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="formMaxAttendees">How many people can attend</InputLabel>
-            <Input
-              id="formMaxAttendees"
-              onChange={onChangeHandler}
-              name="availableSpots"
-              type="number"
-            />
-          </FormControl>
-
-          <Typography></Typography>
-          <FormControl>
-            <FormLabel id="formEventPriceType">Type of event</FormLabel>
-            <RadioGroup
-              aria-labelledby="formEventPriceType"
-              defaultValue="free"
-              name="eventPriceType"
-              onChange={onChangeHandler}
-            >
-              <FormControlLabel value="free" control={<Radio />} label="Free" />
-              <FormControlLabel value="paid" control={<Radio />} label="Paid" />
-            </RadioGroup>
-          </FormControl>
-
-          <Typography>Image URL</Typography>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="formEventImageURL">Add URL to Image</InputLabel>
-            <Input
-              id="formEventImageURL"
-              onChange={onChangeHandler}
-              name="featureImage"
-              value="https://picsum.photos/id/1006/600/600"
-            />
-          </FormControl>
-          <Box sx={{ display: 'flex', gap: '50px' }}>
-            <Link to="/home">
-              <Button variant="outlined">Exit</Button>
-            </Link>
-            <Button variant="contained" type="submit">
-              Create
-            </Button>
+        <Box sx={{position: 'relative'}}>
+          <CardMedia
+            component="img"
+            height={{xs: '194px', md: '462px'}}
+            image={'https://picsum.photos/id/910/800/200'}
+          />
+          <Box onClick={() => navigate(-1)} sx={{position: 'absolute', top: '30px', left: '30px'}}>
+            <BackButton />
+          </Box>
+        </Box>
+        <Box sx={{display: 'flex', flexFlow: 'row', justifyContent: 'center', pt: '40px'}}>
+          <Box
+            component="form"
+            onSubmit={onSubmitHandler}
+            sx={{
+              px: '16px',
+              '& > *': {mb: '24px'},
+              flexGrow: '1',
+              maxWidth: '669px',
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <h3>Create Event</h3>
+            <Card sx={cardStyle}>
+              <h5>Event name</h5>
+              <FormControl fullWidth variant="standard">
+                <InputLabel htmlFor="formEventName">Enter event name</InputLabel>
+                <Input required id="formEventName" onChange={onChangeHandler} name="name" />
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <h5>Select date and time</h5>
+              <FormControl variant="standard">
+                <TextField
+                  required
+                  id="formEventDateTime"
+                  variant="standard"
+                  type="datetime-local"
+                  name="startDate"
+                  sx={{ width: 250 }}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  onChange={onChangeHandler}
+                />
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <FormControl fullWidth>
+                <FormLabel id="demo-radio-buttons-group-label" sx={{color: 'black'}}><h5>Location</h5></FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="Online"
+                  name="type"
+                  onChange={onChangeHandler}
+                >
+                  <FormControlLabel value="Online" control={<Radio />} label="Online" />
+                  <FormControlLabel value="Venue" control={<Radio />} label="Venue" />
+                </RadioGroup>
+                <FormControl variant="standard">
+                  <InputLabel htmlFor="formLinkOrAddress">Platform or Address</InputLabel>
+                  <Input id="formLinkOrAddress" onChange={onChangeHandler} name="location" />
+                </FormControl>
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <h5>Event description</h5>
+              <FormControl fullWidth variant="standard">
+                <InputLabel htmlFor="formEventDescription">Describe your event</InputLabel>
+                <Input id="formEventDescription" onChange={onChangeHandler} name="description" />
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <h5>Max attendees</h5>
+              <FormControl fullWidth variant="standard">
+                <InputLabel htmlFor="formMaxAttendees">How many people can attend</InputLabel>
+                <Input
+                  id="formMaxAttendees"
+                  onChange={onChangeHandler}
+                  name="availableSpots"
+                  type="number"
+                />
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <FormControl>
+                <FormLabel id="formEventPriceType" sx={{color: 'black'}}><h5>Type of event</h5></FormLabel>
+                <RadioGroup
+                  aria-labelledby="formEventPriceType"
+                  defaultValue="free"
+                  name="eventPriceType"
+                  onChange={onChangeHandler}
+                >
+                  <FormControlLabel value="free" control={<Radio />} label="Free" />
+                  <FormControlLabel value="paid" control={<Radio />} label="Paid" />
+                </RadioGroup>
+              </FormControl>
+            </Card>
+            <Card sx={cardStyle}>
+              <h5>Image URL</h5>
+              <FormControl fullWidth variant="standard">
+                <InputLabel htmlFor="formEventImageURL">Add URL to Image</InputLabel>
+                <Input
+                  id="formEventImageURL"
+                  onChange={onChangeHandler}
+                  name="featureImage"
+                  value="https://picsum.photos/id/1006/600/600"
+                />
+              </FormControl>
+            </Card>
+            <Box sx={{ display: 'flex', gap: '20px', mt: '40px', justifyContent: 'space-between' }}>
+              <Button variant="outlined" onClick={() => navigate(-1)} sx={buttonStyle}>Exit</Button>
+              <Button variant="contained" type="submit" sx={buttonStyle}>
+                Create
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Card>
