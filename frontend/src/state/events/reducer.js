8f5e781a0be75bd6e955d036e755afd04a0358e9
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-11-15 20:25:50 -0800

    Added actions for adding/removing attendees to events

--- frontend/src/state/events/reducer.js
+++ frontend/src/state/events/reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_EVENTS, GET_FETCH_EVENTS_DATA_SUCCESS, GET_FETCH_EVENTS_DATA_ERROR } from './types';
+import { FETCH_EVENTS, GET_FETCH_EVENTS_DATA_SUCCESS, GET_FETCH_EVENTS_DATA_ERROR, ADD_USER_TO_EVENT, ADD_USER_TO_EVENT_SUCCESS, ADD_USER_TO_EVENT_ERROR, REMOVE_USER_FROM_EVENT, REMOVE_USER_FROM_EVENT_SUCCESS, REMOVE_USER_FROM_EVENT_ERROR } from './types';
 
 const initialState = {
   loading: false,
@@ -14,6 +14,19 @@ const eventReducer = (state = initialState, action) => {
       return { loading: false, error: null, data: action.payload };
     case GET_FETCH_EVENTS_DATA_ERROR:
       return { loading: false, error: action.payload, data: [] };
+    case ADD_USER_TO_EVENT:
+      return { loading: true, error: null, data: state.data};
+    case ADD_USER_TO_EVENT_SUCCESS:
+      return { loading: false, error: null, data: state.data};
+    case ADD_USER_TO_EVENT_ERROR:
+      return { loading: false, error: action.payload, data: state.data};
+    case REMOVE_USER_FROM_EVENT:
+      return { loading: true, error: null, data: state.data};
+    case REMOVE_USER_FROM_EVENT_SUCCESS:
+      return { loading: false, error: null, data: state.data};
+    case REMOVE_USER_FROM_EVENT_ERROR:
+      return { loading: false, error: action.payload, data: state.data};
+    
     default:
       return state;
   }
