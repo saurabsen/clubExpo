0ec9e01ea831c7f42dc4a7d47e5c6ad0482c05ff
Harshit Punn <harshitpunn@Harshits-MacBook-Air.local>
2022-11-18 18:24:58 -0800

    club-user: ADDED code to update update reports  tab and club admin user flow

--- backend/src/routes/clubRoutes.js
+++ backend/src/routes/clubRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   createClub,
   getClubByMember,
+  getClubByAdmin,
   getClub,
   getClubs,
   updateClub,
@@ -11,6 +12,7 @@ const {
 const { protect } = require('../middleware/authMiddleware');
 
 router.route('/').post(protect, createClub).get(protect, getClubs);
+router.route('/user/:id').get(protect, getClubByAdmin);
 router.route('/members/:userid').get(protect, getClubByMember);
 router
   .route('/:id')
