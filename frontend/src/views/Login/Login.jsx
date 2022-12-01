2a9e40ac65aa4b4e8465dc62f41e5c34e0d79030
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-11-30 23:19:54 -0800

    Fixed mobile view of login page

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -29,60 +29,72 @@ function Login() {
       <Grid
         container
         item
-        xs={4}
+        xs={12}
+        md={4}
         flexDirection="column"
         gap="2rem"
-        sx={{ padding: '16rem 8rem 0 4rem' }}
+        sx={{ alignSelf: 'center', padding: '0 4rem', height: {xs: '60vh', md: '100vh'}, order: 1}}
       >
-        <h1>Login</h1>
-        <form onSubmit={onSubmitHandler}>
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '0.6rem'
-            }}
-          >
-            <TextField
-              required
-              id="standard-required"
-              label="Email"
-              variant="standard"
-              name="email"
-              value={email}
-              onChange={onChangeHandler}
-            />
-            <TextField
-              id="standard-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="standard"
-              name="password"
-              value={password}
-              onChange={onChangeHandler}
-            />
-            <p style={{ fontWeight: 'bold', margin: '1rem 0' }}>
-              <Link to="#">Forgot password?</Link>
-            </p>
+        <Box sx={{display: 'flex', alignItems: 'center', height: '100%', width: '100%'}}>
+          <Box sx={{width: '100%'}}>
+            <h1>Login</h1>
+            <form onSubmit={onSubmitHandler}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '0.6rem'
+                }}
+              >
+                <TextField
+                  required
+                  id="standard-required"
+                  label="Email"
+                  variant="standard"
+                  name="email"
+                  value={email}
+                  onChange={onChangeHandler}
+                />
+                <TextField
+                  id="standard-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  variant="standard"
+                  name="password"
+                  value={password}
+                  onChange={onChangeHandler}
+                />
+                <p style={{ fontWeight: 'bold', margin: '1rem 0' }}>
+                  <Link to="#">Forgot password?</Link>
+                </p>
+              </Box>
+              <Button
+                sx={{
+                  backgroundColor: '#5D1EBF',
+                  padding: '16px 36px 16px 36px',
+                  '&:hover': {
+                    backgroundColor: '#5D1EBF'
+                  }
+                }}
+                variant="contained"
+                type="submit"
+              >
+                Login
+              </Button>
+            </form>
           </Box>
-          <Button
-            sx={{
-              backgroundColor: '#5D1EBF',
-              padding: '16px 36px 16px 36px',
-              '&:hover': {
-                backgroundColor: '#5D1EBF'
-              }
-            }}
-            variant="contained"
-            type="submit"
-          >
-            Login
-          </Button>
-        </form>
+        </Box>
       </Grid>
-      <Grid item xs={8}>
-        <img src={LoginGirl} alt="Login Page Girl" />
+      <Grid item xs={12} md={8} sx={{height: {xs: '30vh', md: '100vh'}, order: {xs: 0, md: 2}}}>
+        <Box sx={{
+          backgroundImage: `url(${LoginGirl})`, 
+          height: '100%', 
+          width: '100%', 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center center'
+        }}>
+        </Box>
       </Grid>
     </Grid>
   );
